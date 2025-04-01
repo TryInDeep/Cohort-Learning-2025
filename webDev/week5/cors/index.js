a13af7e544bcb24a35842eb873a3e5ec1e33c519
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2025-04-01 23:37:32 +0530

    Webdev (#15)
    * added lines
    
    * slide file added
    
    * index and commadfile added
    
    * Promisified function Learning
    
    * Weeks Updated
    
    * Learned Middlewares and Cors

---
+++ webDev/week5/cors/index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const cors = require("cors")
+const app = express();
+
+
+app.use(express.json())
+app.use(cors())
+
+// if you want to block the some domains 
+// app.use(cors({
+//     domains: [""]
+// }))
+
+app.post("/sum", (req,res) => {
+    const a = parseInt(req.body.a)
+    const b = parseInt(req.body.b)
+
+    res.json({
+        answer : a+b
+    })
+})
+
+app.listen(3000)
