67f4cfad94d93eaa113dcc2bf571b9863d3e4c4b
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2025-04-15 23:18:57 +0530

    Webdev (#24)
    * added lines
    
    * slide file added
    
    * index and commadfile added
    
    * Promisified function Learning
    
    * Weeks Updated
    
    * Learned Middlewares and Cors
    
    * Learned about tokens and auth workflow
    
    * Implemented JWT
    
    * connected FE and BE
    
    * Learned about mongoDB, salting, bcrypt
    
    * toDo backend with DB
    
    * Added auth solved some bugs
    
    * Implemented ZOD and Bcrypt
    
    * Course Selling app Created and db and routes created
    
    * userRoute endpointes started

--- webDev/week8CourseSelligApp/index.js
+++ webDev/week8CourseSelligApp/index.js
@@ -1,15 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose")
+
 const {userRouter} = require("./routes/user")
 const {courseRouter} = require("./routes/course")
 const {adminRouter} = require("./routes/admin")
+
 const app  = express();
-app.use(express.json());
+app.use(express.json())
 
 
-app.use("api/v1/user", userRouter)
-app.use("api/v1/course", courseRouter)
-app.use("api/v1/admin", adminRouter)
 
+app.use("/api/v1/user", userRouter)
+app.use("/api/v1/course", courseRouter)
+app.use("/api/v1/admin", adminRouter)
 
-app.listen(3000)
+async function main (){
+   await mongoose.connect("mongodb+srv://Tridip:xpZAnnaxMMUbBQaI@100xdev.upasbqa.mongodb.net/courseSellingApp")
+   app.listen(3000)
+   console.log("Listening on port 3000");
+}
+main();
+
 
--- webDev/week8CourseSelligApp/routes/user.js
+++ webDev/week8CourseSelligApp/routes/user.js
@@ -1,13 +1,50 @@
 const express = require("express");
 const Router = express.Router;
 const userRouter = Router();
+const {userModel} = require("../db");
+const {z} = require("zod");
+const bcrypt = require("bcrypt");
 
 
-
-userRouter.post("/signup", (req , res ) => {
-
+userRouter.post("/signup", async (req , res ) => {
+        const {email, password, firstname, lastname} = req.body;
+        const requiredBody = z.object({
+                email: z.string().min(3).max(100).email(),
+                password: z.string().min().max(100),
+                firstname: z.string().min(3).max(50),
+                lastname: z.string().min(3).max(50)
+              })
+             
+              
+        const parsewithSuccess = requiredBody.safeParse(req.body);
+        if(!parsewithSuccess.success){
+                res.json({
+                        message: "Incorrect format",
+                        error: parsewithSuccess.error
+                })
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        try {
+                await userModel.create({
+                        firstname:firstname,
+                        lastname:lastname,
+                        email:email,
+                        password:hashPassword
+                })
+                res.json({
+                        message: "User is Created "
+                })
+        
+                
+        } catch (error) {
+                res.json({
+                        error: "Something went wrong"
+                })
+        }
 })
+
 userRouter.post("/login", (req , res ) => {
+
 
 })
 
@@ -16,5 +53,5 @@ userRouter.get("/purchases", (req , res ) => {
 })
 
 module.exports = {
-        userRouter : userRouter
+        userRouter
 }
