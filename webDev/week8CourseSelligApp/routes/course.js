1d03823295fa746053e0965c886abd1bb45d3ff8
TryInDeep <164211616+TryInDeep@users.noreply.github.com>
2025-04-17 23:46:53 +0530

    Webdev (#28)
    * added lines
    
    * slide file added
    
    * index and commadfile added
    
    * Promisified function Learning
    
    * Weeks Updated
    
    * Learned Middlewares and Cors
    
    * Learned about tokens and auth workflow
    
    * Implemented JWT
    
    * connected FE and BE
    
    * Learned about mongoDB, salting, bcrypt
    
    * toDo backend with DB
    
    * Added auth solved some bugs
    
    * Implemented ZOD and Bcrypt
    
    * Course Selling app Created and db and routes created
    
    * userRoute endpointes started
    
    * Env file, admin routes and course created
    
    * Admin enpoints completed
    
    * Webdev (#25) (#26)
    
    * added lines
    
    * slide file added
    
    * index and commadfile added
    
    * Promisified function Learning
    
    * Weeks Updated
    
    * Learned Middlewares and Cors
    
    * Learned about tokens and auth workflow
    
    * Implemented JWT
    
    * connected FE and BE
    
    * Learned about mongoDB, salting, bcrypt
    
    * toDo backend with DB
    
    * Added auth solved some bugs
    
    * Implemented ZOD and Bcrypt
    
    * Course Selling app Created and db and routes created
    
    * userRoute endpointes started
    
    * Env file, admin routes and course created
    
    * Admin enpoints completed
    
    * Endpoints completed

--- webDev/week8CourseSelligApp/routes/course.js
+++ webDev/week8CourseSelligApp/routes/course.js
@@ -2,20 +2,41 @@ const express = require("express");
 const Router = express.Router;
 const courseRouter = Router();
 
+const { userMiddleware } = require("../middleware/userMiddleware");
+const { purchaseModel, courseModel } = require("../db");
 
-courseRouter.post("/purchases", (req , res ) => {
+courseRouter.post("/purchases", userMiddleware, async (req, res) => {
+  const userId = req.userId;
+  const courseId = req.body.courseId;
+  try {
+    await purchaseModel.create({
+      userId: userId,
+      purchaseId: courseId,
+    });
     res.json({
-        
-    })
-})
-
-courseRouter.get("/preview", (req , res ) => {
+      message: "Course Purchased",
+    });
+  } catch (error) {
     res.json({
-        
-    })
-})
+      error: "Something went wrong",
+    });
+  }
+});
 
+courseRouter.get("/preview", async (req, res) => {
+  try {
+    const courses = await courseModel.find({});
+    res.json({
+      courses,
+    });
+  } catch (error) {
+    res.json({
+      error: "Something went wrong",
+    });
+  }
+});
 
 module.exports = {
-    courseRouter
-}
+  courseRouter
+};
+
--- webDev/week8CourseSelligApp/routes/user.js
+++ webDev/week8CourseSelligApp/routes/user.js
@@ -1,78 +1,94 @@
 const express = require("express");
 const Router = express.Router;
 const userRouter = Router();
-const { userModel } = require("../db");
+
+const { userModel, purchaseModel } = require("../db");
 const { z } = require("zod");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { JWT_USER_PASSWORD } = require("../config");
+const { userMiddleware } = require("../middleware/userMiddleware");
 
 userRouter.post("/signup", async (req, res) => {
-        const { email, password, firstname, lastname } = req.body;
-        const requiredBody = z.object({
-          email: z.string().min(3).max(100).email(),
-          password: z.string().min().max(100),
-          firstname: z.string().min(3).max(50),
-          lastname: z.string().min(3).max(50),
-        });
-      
-        const parsewithSuccess = requiredBody.safeParse(req.body);
-        if (!parsewithSuccess.success) {
-          res.json({
-            message: "Incorrect format",
-            error: parsewithSuccess.error,
-          });
-        }
-        const hashPassword = await bcrypt.hash(password, 5);
-        try {
-          await userModel.create({
-            firstname: firstname,
-            lastname: lastname,
-            email: email,
-            password: hashPassword,
-          });
-          res.json({
-            message: "User is Created ",
-          });
-        } catch (error) {
-          res.json({
-            error: "Something went wrong",
-          });
-        }
-      });
-      
-      userRouter.post("/login", async (req, res) => {
-        const { email, password } = req.body;
-        try {
-          const user = await userModel.findOne({
-            email: email,
-          });
-          const decodedPassword = await bcrypt.compare(password, user.password);
-      
-          if (decodedPassword) {
-            const token = jwt.sign(
-              {
-                id: user._id,
-              },
-              JWT_USER_PASSWORD
-            );
-      
-            res.json({
-              token: token,
-            });
-          } else {
-            res.json({
-              message: "Invalid Credentials",
-            });
-          }
-        } catch (error) {
-          res.json({
-            error: "Something went wrong",
-          });
-        }
-      });
+  const { email, password, firstname, lastname } = req.body;
+  const requiredBody = z.object({
+    email: z.string().min(3).max(100).email(),
+    password: z.string().min().max(100),
+    firstname: z.string().min(3).max(50),
+    lastname: z.string().min(3).max(50),
+  });
 
-userRouter.get("/purchases", (req, res) => {});
+  const parsewithSuccess = requiredBody.safeParse(req.body);
+  if (!parsewithSuccess.success) {
+    res.json({
+      message: "Incorrect format",
+      error: parsewithSuccess.error,
+    });
+  }
+  const hashPassword = await bcrypt.hash(password, 5);
+  try {
+    await userModel.create({
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      password: hashPassword,
+    });
+    res.json({
+      message: "User is Created ",
+    });
+  } catch (error) {
+    res.json({
+      error: "Something went wrong",
+    });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({
+      email: email,
+    });
+    const decodedPassword = await bcrypt.compare(password, user.password);
+
+    if (decodedPassword) {
+      const token = jwt.sign(
+        {
+          id: user._id,
+        },
+        JWT_USER_PASSWORD
+      );
+
+      res.json({
+        token: token,
+      });
+    } else {
+      res.json({
+        message: "Invalid Credentials",
+      });
+    }
+  } catch (error) {
+    res.json({
+      error: "Something went wrong",
+    });
+  }
+});
+
+userRouter.get("/purchases", userMiddleware, async (req, res) => {
+  const userId = req.userId;
+  try {
+    const purchaseItems = await purchaseModel.find({
+      userId,
+    });
+    res.json({
+      Purchesed: purchaseItems,
+    });
+  } catch (error) {
+    res.json({
+      error: "Something went wrong",
+    });
+  }
+});
 
 module.exports = {
   userRouter
